7da4b56515c4a2744a07e90baa1a678052c00c02
Daniel Franzén <Daniel@Daniels-MBP-2.lan>
2019-06-19 21:51:54 +0200

    Added Search component Fixed search in api Shows result after search

---
+++ src/components/GSearch.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <input
+      v-model="queryString"
+      @keyup.enter="search"
+      type="search"
+      placeholder="Search for gif"
+      class="search-field"
+    >
+    <div class="search-results">
+      <img
+        v-for="result in searchResults"
+        :key="result.id"
+        :src="result.images.fixed_width.url"
+        class="search-result__item"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+
+export default {
+  name: 'GSearch',
+  data() {
+    return {
+      queryString: '',
+      searchResults: []
+    };
+  },
+  methods: {
+    search() {
+      api.search(this.queryString).then(response => {
+        this.searchResults = response.data;
+      });
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.search-field {
+  -webkit-appearance: none;
+  width: 100%;
+  max-width: 500px;
+  height: 40px;
+  border-radius: 4px;
+  border: solid 1px #383a3c;
+  margin-bottom: 30px;
+  padding: 5px;
+
+  &:focus {
+    outline: 0;
+  }
+}
+.search-result {
+  display: flex;
+  flex-wrap: wrap;
+
+  &__item {
+    padding: 5px;
+  }
+}
+</style>
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
-import config from '@/src/config.js';
+import config from '../config.js';
 const baseUrl = 'https://api.giphy.com/v1/gifs/';
 
 export default {
   search(queryString) {
     const queryType = 'search';
-    const url = `${baseUrl}${queryType}?${config.apiKey}&q=${queryString}&limit=10&rating=g`;
+    const url = `${baseUrl}${queryType}?api_key=${
+      config.apiKey
+    }&q=${queryString}&limit=10&rating=g`;
     const result = fetch(url).then(response => response.json());
 
     return result;
