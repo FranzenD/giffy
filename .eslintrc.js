4fb990341cf0641abfd693b5dc5fc0c4979b7e04
Daniel Franzén <Daniel@Daniels-MBP-2.lan>
2019-06-19 22:16:43 +0200

    Added lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,19 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': ['error', { singleQuote: true }]
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
